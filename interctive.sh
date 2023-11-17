6d98903a2152e197d04e99ca6305e07a9118cc00
mapa <marciopereiraaraujo1997d1@gmail.com>
2023-11-17 15:02:42 -0300

    read user input

---
+++ interctive.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo hello what is your name?
+read first_name
+echo what is your last name?
+read last_name
+
+echo hello and welcome $first_name $last_name
---
+++ shel_variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+my_name=john
+my_age=80
+
+echo hello $my_name, now you are $my_age
