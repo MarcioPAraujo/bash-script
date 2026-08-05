f4d252bd99c99197adea4ff69750ae07c3029aa1
Marcio <marciopereiraaraujo1997d1@gmail.com>
2026-08-05 10:46:08 -0300

    feat: case

---
+++ case.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+case $1 in
+	1) echo "January" ;;
+	2) echo "February" ;;
+	3) echo "March" ;;
+	4) echo "April" ;;
+	5) echo "May" ;;
+	6) echo "June" ;;
+	7) echo "July" ;;
+	8) echo "August" ;;
+	9) echo "September" ;;
+	10) echo "December" ;;
+	11) echo "November" ;;
+	12) echo "December" ;;
+	*) echo "this month does not exixts" ;;
+esac
+
+exit 0
